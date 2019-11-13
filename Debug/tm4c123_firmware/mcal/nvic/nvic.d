5847a3821bc9c363f15304af3f5047c130b39aed
Mraafat32 <muhammadraafatrashad@gmail.com>
2019-11-13 17:55:43 +0200

    Create nvic.d

---
+++ Debug/tm4c123_firmware/mcal/nvic/nvic.d
@@ -0,0 +1,15 @@
+# FIXED
+
+tm4c123_firmware/mcal/nvic/nvic.obj: ../tm4c123_firmware/mcal/nvic/nvic.c
+tm4c123_firmware/mcal/nvic/nvic.obj: ../tm4c123_firmware/mcal/nvic/../../utils/STD_Types.h
+tm4c123_firmware/mcal/nvic/nvic.obj: ../tm4c123_firmware/mcal/nvic/../../utils/Bit_Math.h
+tm4c123_firmware/mcal/nvic/nvic.obj: ../tm4c123_firmware/mcal/nvic/../../config/nvic_cfg.h
+tm4c123_firmware/mcal/nvic/nvic.obj: ../tm4c123_firmware/mcal/nvic/../mcu_hw.h
+tm4c123_firmware/mcal/nvic/nvic.obj: ../tm4c123_firmware/mcal/nvic/nvic.h
+
+../tm4c123_firmware/mcal/nvic/nvic.c: 
+../tm4c123_firmware/mcal/nvic/../../utils/STD_Types.h: 
+../tm4c123_firmware/mcal/nvic/../../utils/Bit_Math.h: 
+../tm4c123_firmware/mcal/nvic/../../config/nvic_cfg.h: 
+../tm4c123_firmware/mcal/nvic/../mcu_hw.h: 
+../tm4c123_firmware/mcal/nvic/nvic.h: 
