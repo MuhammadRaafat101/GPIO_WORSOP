d9d86ead1d3dbec886f2f44a3c1f14a2f3a038f8
Mraafat32 <muhammadraafatrashad@gmail.com>
2019-11-13 17:54:23 +0200

    Create i2c_lcfg.d

---
+++ Debug/tm4c123_firmware/config/i2c_lcfg.d
@@ -0,0 +1,11 @@
+# FIXED
+
+tm4c123_firmware/config/i2c_lcfg.obj: ../tm4c123_firmware/config/i2c_lcfg.c
+tm4c123_firmware/config/i2c_lcfg.obj: ../tm4c123_firmware/config/../utils/STD_types.h
+tm4c123_firmware/config/i2c_lcfg.obj: ../tm4c123_firmware/config/i2c_cfg.h
+tm4c123_firmware/config/i2c_lcfg.obj: ../tm4c123_firmware/config/../mcal/i2c/i2c_types.h
+
+../tm4c123_firmware/config/i2c_lcfg.c: 
+../tm4c123_firmware/config/../utils/STD_types.h: 
+../tm4c123_firmware/config/i2c_cfg.h: 
+../tm4c123_firmware/config/../mcal/i2c/i2c_types.h: 
