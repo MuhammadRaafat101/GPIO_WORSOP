8da036c2d6a766c568f88e32f39bbf658ea4e856
Mraafat32 <muhammadraafatrashad@gmail.com>
2019-11-13 17:54:51 +0200

    Create subdir_vars.mk

---
+++ Debug/tm4c123_firmware/hal/accelerometer/subdir_vars.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../tm4c123_firmware/hal/accelerometer/accelerometer.c 
+
+C_DEPS += \
+./tm4c123_firmware/hal/accelerometer/accelerometer.d 
+
+OBJS += \
+./tm4c123_firmware/hal/accelerometer/accelerometer.obj 
+
+OBJS__QUOTED += \
+"tm4c123_firmware\hal\accelerometer\accelerometer.obj" 
+
+C_DEPS__QUOTED += \
+"tm4c123_firmware\hal\accelerometer\accelerometer.d" 
+
+C_SRCS__QUOTED += \
+"../tm4c123_firmware/hal/accelerometer/accelerometer.c" 
+
+
