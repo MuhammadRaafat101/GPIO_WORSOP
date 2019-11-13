76f80319f2bc558b6eb91b98cc44b9530ec854bb
Mraafat32 <muhammadraafatrashad@gmail.com>
2019-11-13 17:54:09 +0200

    Create lab_01_spi.d

---
+++ Debug/tm4c123_firmware/appl/lab_01_spi.d
@@ -0,0 +1,19 @@
+# FIXED
+
+tm4c123_firmware/appl/lab_01_spi.obj: ../tm4c123_firmware/appl/lab_01_spi.c
+tm4c123_firmware/appl/lab_01_spi.obj: ../tm4c123_firmware/appl/../mcal/global_interrupt.h
+tm4c123_firmware/appl/lab_01_spi.obj: ../tm4c123_firmware/appl/../mcal/sys_ctr/SysCtr.h
+tm4c123_firmware/appl/lab_01_spi.obj: ../tm4c123_firmware/appl/../mcal/nvic/nvic.h
+tm4c123_firmware/appl/lab_01_spi.obj: ../tm4c123_firmware/appl/../mcal/port_driver/port.h
+tm4c123_firmware/appl/lab_01_spi.obj: ../tm4c123_firmware/appl/../mcal/spi/spi.h
+tm4c123_firmware/appl/lab_01_spi.obj: ../tm4c123_firmware/appl/../mcal/spi/../../utils/STD_Types.h
+tm4c123_firmware/appl/lab_01_spi.obj: ../tm4c123_firmware/appl/../mcal/spi/spi_types.h
+
+../tm4c123_firmware/appl/lab_01_spi.c: 
+../tm4c123_firmware/appl/../mcal/global_interrupt.h: 
+../tm4c123_firmware/appl/../mcal/sys_ctr/SysCtr.h: 
+../tm4c123_firmware/appl/../mcal/nvic/nvic.h: 
+../tm4c123_firmware/appl/../mcal/port_driver/port.h: 
+../tm4c123_firmware/appl/../mcal/spi/spi.h: 
+../tm4c123_firmware/appl/../mcal/spi/../../utils/STD_Types.h: 
+../tm4c123_firmware/appl/../mcal/spi/spi_types.h: 
