b659941113b214dd6b77c426246cd033ab7883d1
Mraafat32 <muhammadraafatrashad@gmail.com>
2019-11-13 17:56:18 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,33 @@
+
+/*
+ * lab_03_spi.c
+ *
+ *  Created on: Aug 20, 2019
+ *      Author: Muhammad.Elzeiny
+ */
+/*
+#include "../mcal/global_interrupt.h"
+#include "../mcal/sys_ctr/SysCtr.h"
+#include "../mcal/port_driver/port.h"
+#include "../mcal/dio/dio.h"
+*/
+
+extern void lab_00_dio(void);
+
+void main(void)
+{
+ /*   SysCtr_init();
+    PORT_init();
+
+    Dio_WriteChannel(Dio_Channel_F0, STD_high);
+    Dio_WriteChannel(Dio_Channel_F1, STD_high);
+    Dio_WriteChannel(Dio_Channel_F2, STD_high);
+
+    while(1)
+    {
+
+    }*/
+    lab_00_dio();
+}
+
+
