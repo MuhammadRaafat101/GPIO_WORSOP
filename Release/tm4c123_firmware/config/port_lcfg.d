97aa2de5ad8e27a0b73324a1d03fb5f17c9d51aa
Mraafat32 <muhammadraafatrashad@gmail.com>
2019-11-13 17:57:23 +0200

    Create port_lcfg.d

---
+++ Release/tm4c123_firmware/config/port_lcfg.d
@@ -0,0 +1,11 @@
+# FIXED
+
+tm4c123_firmware/config/port_lcfg.obj: ../tm4c123_firmware/config/port_lcfg.c
+tm4c123_firmware/config/port_lcfg.obj: ../tm4c123_firmware/config/../mcal/port_driver/port_types.h
+tm4c123_firmware/config/port_lcfg.obj: ../tm4c123_firmware/config/../mcal/port_driver/../../utils/STD_Types.h
+tm4c123_firmware/config/port_lcfg.obj: ../tm4c123_firmware/config/port_cfg.h
+
+../tm4c123_firmware/config/port_lcfg.c: 
+../tm4c123_firmware/config/../mcal/port_driver/port_types.h: 
+../tm4c123_firmware/config/../mcal/port_driver/../../utils/STD_Types.h: 
+../tm4c123_firmware/config/port_cfg.h: 
