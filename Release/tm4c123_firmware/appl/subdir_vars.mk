398cdaa1fb3c9f00cb57440a349e1a6a99873fd9
Mraafat32 <muhammadraafatrashad@gmail.com>
2019-11-13 17:57:12 +0200

    Create subdir_vars.mk

---
+++ Release/tm4c123_firmware/appl/subdir_vars.mk
@@ -0,0 +1,32 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../tm4c123_firmware/appl/lab_00_dio.c \
+../tm4c123_firmware/appl/lab_01_spi.c 
+
+C_DEPS += \
+./tm4c123_firmware/appl/lab_00_dio.d \
+./tm4c123_firmware/appl/lab_01_spi.d 
+
+OBJS += \
+./tm4c123_firmware/appl/lab_00_dio.obj \
+./tm4c123_firmware/appl/lab_01_spi.obj 
+
+OBJS__QUOTED += \
+"tm4c123_firmware\appl\lab_00_dio.obj" \
+"tm4c123_firmware\appl\lab_01_spi.obj" 
+
+C_DEPS__QUOTED += \
+"tm4c123_firmware\appl\lab_00_dio.d" \
+"tm4c123_firmware\appl\lab_01_spi.d" 
+
+C_SRCS__QUOTED += \
+"../tm4c123_firmware/appl/lab_00_dio.c" \
+"../tm4c123_firmware/appl/lab_01_spi.c" 
+
+
