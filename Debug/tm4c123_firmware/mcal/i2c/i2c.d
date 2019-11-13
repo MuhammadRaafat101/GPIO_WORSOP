fb135d52e2c7bce793986ed87ad885de9e6bf111
Mraafat32 <muhammadraafatrashad@gmail.com>
2019-11-13 17:55:33 +0200

    Create i2c.d

---
+++ Debug/tm4c123_firmware/mcal/i2c/i2c.d
@@ -0,0 +1,17 @@
+# FIXED
+
+tm4c123_firmware/mcal/i2c/i2c.obj: ../tm4c123_firmware/mcal/i2c/i2c.c
+tm4c123_firmware/mcal/i2c/i2c.obj: ../tm4c123_firmware/mcal/i2c/../../utils/Bit_Math.h
+tm4c123_firmware/mcal/i2c/i2c.obj: ../tm4c123_firmware/mcal/i2c/../../utils/STD_Types.h
+tm4c123_firmware/mcal/i2c/i2c.obj: ../tm4c123_firmware/mcal/i2c/../mcu_hw.h
+tm4c123_firmware/mcal/i2c/i2c.obj: ../tm4c123_firmware/mcal/i2c/../../config/i2c_cfg.h
+tm4c123_firmware/mcal/i2c/i2c.obj: ../tm4c123_firmware/mcal/i2c/i2c_types.h
+tm4c123_firmware/mcal/i2c/i2c.obj: ../tm4c123_firmware/mcal/i2c/i2c.h
+
+../tm4c123_firmware/mcal/i2c/i2c.c: 
+../tm4c123_firmware/mcal/i2c/../../utils/Bit_Math.h: 
+../tm4c123_firmware/mcal/i2c/../../utils/STD_Types.h: 
+../tm4c123_firmware/mcal/i2c/../mcu_hw.h: 
+../tm4c123_firmware/mcal/i2c/../../config/i2c_cfg.h: 
+../tm4c123_firmware/mcal/i2c/i2c_types.h: 
+../tm4c123_firmware/mcal/i2c/i2c.h: 
