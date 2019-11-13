7ee2d1e8648e1fe09f2060c478ffb37308453328
Mraafat32 <muhammadraafatrashad@gmail.com>
2019-11-13 17:54:33 +0200

    Create spi_lcfg.d

---
+++ Debug/tm4c123_firmware/config/spi_lcfg.d
@@ -0,0 +1,11 @@
+# FIXED
+
+tm4c123_firmware/config/spi_lcfg.obj: ../tm4c123_firmware/config/spi_lcfg.c
+tm4c123_firmware/config/spi_lcfg.obj: ../tm4c123_firmware/config/../utils/STD_types.h
+tm4c123_firmware/config/spi_lcfg.obj: ../tm4c123_firmware/config/spi_cfg.h
+tm4c123_firmware/config/spi_lcfg.obj: ../tm4c123_firmware/config/../mcal/spi/spi_types.h
+
+../tm4c123_firmware/config/spi_lcfg.c: 
+../tm4c123_firmware/config/../utils/STD_types.h: 
+../tm4c123_firmware/config/spi_cfg.h: 
+../tm4c123_firmware/config/../mcal/spi/spi_types.h: 
